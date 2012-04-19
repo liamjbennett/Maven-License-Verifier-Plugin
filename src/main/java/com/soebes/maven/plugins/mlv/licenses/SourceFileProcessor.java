3362ce2002304c6f8fa1e4b7bda2262f611fe4a9
lbennett <lbennett@mimecast.com>
2012-04-19 17:30:04 +0100

    WIP for source scanning

---
+++ src/main/java/com/soebes/maven/plugins/mlv/licenses/SourceFileProcessor.java
@@ -0,0 +1,11 @@
+package com.soebes.maven.plugins.mlv.licenses;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lbennett
+ * Date: 17/04/2012
+ * Time: 11:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class SourceFileProcessor {
+}
